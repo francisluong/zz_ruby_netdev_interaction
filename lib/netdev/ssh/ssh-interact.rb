d0c6ad900c106248c9b53ac56c6a2d5a73dfb626
Francis Luong (Franco) <fluong@salesforce.com>
2014-08-12 21:02:03 -0700

    modified sendline not to wait for prompt

--- lib/netdev/ssh/ssh-interact.rb
+++ lib/netdev/ssh/ssh-interact.rb
@@ -2,59 +2,59 @@ require "net/ssh"
 require "net/ssh/telnet"
 
 module NetDev
-class SSH
+  class SSH
     attr_accessor :prompt_re
 
     def initialize(user, passwd)
-        @user = user
-        @passwd = passwd
-        @prompt_re = /^([@a-zA-Z0-9\.\-\_]+[>#%$])/
-        @reset_timeout_on_newlines = true
-        @timeout_sec = 10
-        @wait_sec = 0.01
-        @quiet = false
-        @received_text = ""
+      @user = user
+      @passwd = passwd
+      @prompt_re = /^([@a-zA-Z0-9\.\-\_]+[>#%$])/
+      @reset_timeout_on_newlines = true
+      @timeout_sec = 10
+      @wait_sec = 0.01
+      @quiet = false
+      @received_text = ""
     end
 
     def connect(hostname, port=22)
-        #connect to SSH target and open channel
-        @ssh = Net::SSH.start(hostname,
-            @user,
-            :port => port,
-            :password => @passwd)
-        @session = Net::SSH::Telnet.new(
-            "Session" => @ssh,
-            "Prompt" => @prompt_re
-        )
+      #connect to SSH target and open channel
+      @ssh = Net::SSH.start(hostname,
+                            @user,
+                            :port => port,
+                            :password => @passwd)
+      @session = Net::SSH::Telnet.new(
+          "Session" => @ssh,
+          "Prompt" => @prompt_re
+      )
     end
 
     def wait_for_prompt
-        return self.wait_for_regex @prompt_re
+      self.wait_for_regex @prompt_re
     end
 
     def wait_for_regex(expression)
-        #Loop receive and wait until a regular expression is matched in output
-        @received_text << @session.wait_for(
-            "Match" => expression,
-            "Timeout" => @timeout,
-            "waittime" => @wait_sec
-        )
+      #Loop receive and wait until a regular expression is matched in output
+      @session.waitfor(
+          "Match" => expression,
+          "Timeout" => @timeout_sec,
+          "waittime" => @wait_sec
+      )
     end
 
     def sendline(single_command)
-        #send a single line and wait for prompt
-        @received_text = @session.cmd(single_command)
-        if not @quiet
-            print @received_text
-        end
+      #send a single line, but do not wait for prompt
+      @session.puts(single_command)
+
     end
 
     def send(textblock)
-        #send a series of lines, wait for prompt after each
-        textblock.lines do |line|
-            self.sendline(line.chomp)
-        end
+      #send a series of lines, wait for prompt after each
+      textblock.lines do |line|
+        self.sendline(line.chomp)
+        @received_text = self.wait_for_prompt
+        print @received_text unless @quiet
+      end
     end
 
-end
+  end
 end
